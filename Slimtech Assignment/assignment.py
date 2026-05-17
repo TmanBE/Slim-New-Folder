d7b109b502f9306d7a94921a5e6b0d65126b982a
TmanBE <fadisonolusegun@gmail.com>
2026-05-17 20:16:24 +0100

    New Assignment added

---
+++ Slimtech Assignment/assignment.py
@@ -0,0 +1,23 @@
+#Brief History on Nigeria
+print("Nigeria is a country located in West Africa. It has a rich history that dates back to ancient times. The area now known as Nigeria was home to several powerful empires and kingdoms, including the Nok culture, the Kingdom of Benin, and and Yoruba Oyo Empire as well as the sokoto caliphate. The modern nation-state was created by British colonization, with the protectorates of Northern and Southern Nigeria amalgamated in 1914\n")
+print("In the 19th century, Nigeria became a British colony, and it gained independence in 1960. Since then, Nigeria has experienced a mix of political stability and instability, with periods of military rule and civilian government. The country has also faced various challenges, including economic struggles, corruption, and security issues.\n")
+print("A stable democratic government was finally re-established in 1999.\n")
+
+#Ethnic Groups in Nigeria
+print("Nigeria is a diverse country with over 250 ethnic groups. The three largest ethnic groups are the Hausa, Yoruba, and Igbo, each making up a significant portion of the population.\n")
+print("The Hausa people are predominantly found in the northern part of Nigeria and are known for their rich cultural heritage, including music, art, and architecture. They are also predominantly Muslim.\n")
+print("The Yoruba people are primarily located in the southwestern part of Nigeria and have a rich cultural heritage that includes music, art, and religion. They are predominantly Christian, but there is also a significant Muslim population among the Yoruba.\n")
+print("The Igbo people are mainly found in the southeastern part of Nigeria and are known for their entrepreneurial spirit and rich cultural traditions. They are predominantly Christian, with a small Muslim population.")
+print("In addition to these three major ethnic groups, Nigeria is home to many other smaller ethnic groups, each with their own unique cultures and traditions. The official language is English.\n")
+
+#The Economy of Nigeria
+print("Nigeria used to be the largest economy in Africa and is classified as a mixed economy. The country is rich in natural resources, particularly oil and gas, which have been the main drivers of its economy. However, Nigeria's economy has faced challenges such as corruption, mismanagement, and a heavy reliance on oil exports, which has made it vulnerable to fluctuations in global oil prices.\n")
+
+#The Government & President of Nigeria
+print("Nigeria operates as a federal republic with a presidential system of government. The President of Nigeria is Bola Ahmed Tinubu, who has been in office since 2023.\n")
+
+#Other Information about Nigeria
+print("The currency of Nigeria is the Nigerian Naira (NGN). The country has a population of over 200 million people, making it the most populous country in Africa. The land size of Nigeria is approximately 923,768 square kilometers (356,669 square miles). It ranks 32nd largest country in the world (about twice the size of California) and the capital city is Abuja.\n")
+
+#Positives about Nigeria
+print("Nigeria has a rich cultural heritage, with diverse traditions, languages, and arts. The country is known for its vibrant music and film industries, which have gained international recognition. Nigeria also has a large and growing economy, with a young and dynamic population that contributes to its potential for growth and development.")
