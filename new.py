ce62d1f28b4ee4e957903211333cc87da1b4bafa
TmanBE <fadisonolusegun@gmail.com>
2026-05-17 20:33:02 +0100

    random addition

---
+++ new.py
@@ -0,0 +1,16 @@
+name = "Tolulope"
+age = 38
+email = "fadiyatolulope@gmail.com"
+is_raining = False
+temperature = 30.0
+#print("My name is", name + ", i am", age, "yrs old and the weather is", temperature, "degrees and the raining status is", is_raining)
+#print("\n")
+#print(f"My name is {name}, \ni am {age} yrs old \nand the weather is {temperature} degrees \nand the raining status is {is_raining}")
+
+#concatenation
+first_name = "Tolulope"
+last_name = "Fadiya"
+full_name = first_name + " " + last_name
+print(full_name)
+print("\n")
+print(f"My full name is {full_name}")
